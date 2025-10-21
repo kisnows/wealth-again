ccb79e693e3cae5822a26b90f1d1f6b5242260af
kisnows <yq12315@gmail.com>
2025-10-21 16:35:51 +0800

    feat: update middleware matcher to exclude additional routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 export { auth as middleware } from "@/server/auth";
 
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|public).*)"],
+  matcher: [
+    "/((?!api/auth|api/v1/auth|signin|_next/static|_next/image|favicon.ico|public).*)",
+  ],
 };
